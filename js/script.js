ba59d61e345dc8730eb9b55039ebc9c312dd2458
Simon Mazurek <dev.mazureq@gmail.com>
2023-02-20 20:26:18 +0100

    add add active class to clicked element

--- js/script.js
+++ js/script.js
@@ -4,19 +4,24 @@
     console.log('links:', links);
   });*/
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /*[DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
 }
   
-    /* add class 'active' to the clicked link */
+    /*[IN PROGRESS] add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement);
+    //console.log('clickedElement (with plus): ' + clickedElement);
+    clickedElement.classList.add('active');
+
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
