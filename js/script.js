9e9af6d6b1c524b952b01c3a2fb846a47a736260
Simon Mazurek <dev.mazureq@gmail.com>
2023-02-21 20:59:49 +0100

    add generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+{'use strict';
 /*document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
@@ -46,3 +46,43 @@
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+  /* Generate a list of article titles*/
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList=document.querySelector(optTitleListSelector).innerHTML = '';
+  console .log ('title list:',titleList);
+
+
+  /* for each article */
+  
+  let html = '';
+  const articles= document.querySelector(optArticleSelector);
+  for (let article of articles){
+
+
+    
+    /* get the article id */
+    const articleId=article.getAttribute('href');
+    console.log('getAttribute:',articleId);
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('link html:',linkHTML);
+
+    /* insert link into titleList */
+    html = html + linkHTML;
+  }
+  titleList.innerHTML = html;
+}
+
+generateTitleLinks();
+}
